1fae750ef725a664eaa5f9c164d16b8e3be4e9fd
fgy <fgy@dankal.cn>
2018-03-27 18:02:07 +0800

    成功响应

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,22 +2,34 @@
 namespace app\index\controller;
 
 use think\Controller;
+use app\index\Controller\Send;
 use think\Request;
 use think\Response;
-use app\index\Send;
+
 
 class Index extends  Controller
 {
+    use Send;
+
+    public function __construct()
+    {
+
+    }
+
     public function index() {
         $result = file_get_contents("php://input");
 //        var_dump($result);die();
-        /*if(empty($result)){
-            echo 101;
-            return;
-        }*/
+        if(empty($result)){
+            $this->returnmsg(400);
+        }
         $json = json_decode($result,true);
-        $result =  model("Datas")->getData($json);
-        $this->returnmsg(500,[],[],'Internal Server Error',"Sql error","绑定失败~");
+        $results =  model("Datas")->getData($json);
+        if(empty($results)){
+            $this->returnmsg(500,[],[],'Internal Server Error',"Sql error","绑定失败~");
+        }else{
+            $this->render(200,$results);
+        }
+
         //exit;
     }
 }
